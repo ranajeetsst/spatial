3d2bc7c48eb9de3ed45f4ce597732c213557a60d
jitendra <148371611+ranajeetsst@users.noreply.github.com>
2023-10-27 19:06:39 +0530

    Update polygons.js
    remove commant

--- app/models/polygons/polygons.js
+++ app/models/polygons/polygons.js
@@ -1,17 +1,3 @@
-// const mongoose = require("mongoose");
-
-// var polygonSchema = new mongoose.Schema(
-//   {
-//     type: { type: String, enum: ['Polygon'], required: true },
-//     coordinates: { type: [[Number]], required: true }, // [[[ring1], [ring2], ...]] for a polygon
-//   },
-//   { timestamps: true, versionKey: false }
-// ); 
-// polygonSchema.index({ coordinates: '2dsphere' });
-// const Polygon  = mongoose.model("Polygons", polygonSchema);
-
-// module.exports = Polygon;
-
 const mongoose = require("mongoose");
 
 var polygonSchema = new mongoose.Schema(
